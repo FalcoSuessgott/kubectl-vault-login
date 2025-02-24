74b8c70b238adfc8db1d08a7bf5708011761271d
Tom Morelly <tom.morelly@clearroute.io>
2025-02-25 08:57:15 +1100

    feat(kubeconfig): patch kubeconfig (#10)

--- pkg/exec_credential/exec_credential.go
+++ pkg/exec_credential/exec_credential.go
@@ -5,16 +5,16 @@ import (
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/pkg/apis/clientauthentication/v1beta1"
+	"k8s.io/client-go/pkg/apis/clientauthentication/v1"
 )
 
 func NewExecCredential(token string, expiry time.Time) ([]byte, error) {
-	execCred := v1beta1.ExecCredential{
+	execCred := v1.ExecCredential{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: "client.authentication.k8s.io/v1beta1",
+			APIVersion: "client.authentication.k8s.io/v1",
 			Kind:       "ExecCredential",
 		},
-		Status: &v1beta1.ExecCredentialStatus{
+		Status: &v1.ExecCredentialStatus{
 			Token:               token,
 			ExpirationTimestamp: &metav1.Time{Time: expiry},
 		},
